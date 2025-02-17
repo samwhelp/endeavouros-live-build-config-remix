8a92080c3bc21ff06188ab9f3e1f27542b807e79
samwhelp <samwhelp@users.noreply.github.com>
2025-02-17 10:48:41 +0800

    fix

---
+++ sample/build/prepare.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Get mirrorlist for offline installs
+wget -qN --show-progress -P "airootfs/etc/pacman.d/" "https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-ISO/main/mirrorlist"
+
+# Get wallpaper for installed system
+wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/endeavouros-team/Branding/master/backgrounds/endeavouros-wallpaper.png"
+
+
+# Make sure build scripts are executable
+chmod +x "./"{"mkarchiso","run_before_squashfs.sh"}
+
+get_pkg() {
+    sudo pacman -Syw "$1" --noconfirm --cachedir "airootfs/root/packages" \
+    && sudo chown $USER:$USER "airootfs/root/packages/"*".pkg.tar"*
+}
+
+get_pkg "eos-settings-plasma"
+
+# Build liveuser skel
+cd "airootfs/root/endeavouros-skel-liveuser"
+makepkg -f
